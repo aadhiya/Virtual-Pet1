1c9283767e7951baa07a4d60bea153338d21f1e8
Taabish77 <69813169+Taabish77@users.noreply.github.com>
2020-10-10 17:38:11 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var dog, happyDog;
+var database;
+var foodS, foodStock;
+var foodStock= 24;
+function preload()
+{
+dog = loadImage("Dog.png");
+happyDog = loadImage("happyDog.png");
+}
+
+function setup() {
+  
+  database = firebase.database();
+  console.log(database);
+  createCanvas(800, 700);
+  dog = createSprite(400,300);
+}
+
+
+function draw() {  
+  background("green")
+if(keywentDown(UP_ARROW)){
+ writeStock(foodS);
+dog.addImage(happyDog);
+}
+if(keywentDown(UP_ARROW)){
+  var foodStock-1;
+ }
+  drawSprites();
+  text("foodStock"+score,width-300,50);
+  noStroke();
+  textSize(35);
+  fill("white")
+
+}
+function readStock(data){
+foodS=data.val();
+}
+
+function writeStock(x){
+
+database.ref('/').update({
+Food:x
+})
+}
+
